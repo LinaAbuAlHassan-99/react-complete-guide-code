c26dff41fc00c0fa2bc60d9a48a910160c22591c
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-06-30 20:20:06 +0300

    callback and memo is rely good to optimization

--- code/07-finished/src/App.js
+++ code/07-finished/src/App.js
@@ -1,4 +1,23 @@
 import React, { useState, useCallback, useMemo } from 'react';
+/* 
+vidio 20+19
+state created only on the first time the comp created so it just update it value
+it will be recrated only when i render something on condetion like 
+from parent i want to call button if something hapend
+so the state for button will new create when true else will delete the stat so not render in the screen
+//Scadual update 
+// when i depend in the previous state i need to use the function  when
+ i set the state so i dont mess the change and get the latest state 
+ //setEffect work in the same way kep trach on the state change when other value change
+ byt in state in the same value change
+// when i call the state update function(,second arumant) state not 
+updated emeditly no it scadualed when re run the comp will have the new state 
+
+//when i use 2 state  update after each other //in the same function ->
+// react will tack this two update and patch them to one update state so one schadual update change
+
+*/
+//vidio 22 last one
 
 import './App.css';
 import DemoList from './components/Demo/DemoList';
--- code/07-finished/src/components/Demo/DemoList.js
+++ code/07-finished/src/components/Demo/DemoList.js
@@ -3,12 +3,20 @@ import React, { useMemo } from 'react';
 import classes from './DemoList.module.css';
 
 const DemoList = (props) => {
+// i do sorting
+//const sortedList = props.items.sort((//a,b)=>a-b)
   const { items } = props;
-
+  // title will change if click change title so i need to reevaluate this comp 
+  // but sort is not need to re evaluate ecuase it still sorted and sort are costly
+  // so i use useMemo on input to this will never change untill input change like call back
+  //just when new item added i need to re sort so i trach on it
+  //items is an array so alwyse will re create it in App i need to useMemo in App.js the array i define it fixed by mu hand so nothing will chane so in seconde parameter empty
+  
   const sortedList = useMemo(() => {
     console.log('Items sorted');
     return items.sort((a, b) => a - b);
   }, [items]); 
+
   console.log('DemoList RUNNING');
 
   return (
