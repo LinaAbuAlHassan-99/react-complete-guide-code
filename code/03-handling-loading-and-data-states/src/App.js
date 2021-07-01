30c125df3a20a78fae246b02321a9abb04bcf8d8
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-07-01 17:48:19 +0300

    https request with async and await

--- code/03-handling-loading-and-data-states/src/App.js
+++ code/03-handling-loading-and-data-states/src/App.js
@@ -1,15 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
-
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+// still async
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
+  /* we are sending i get(select) request whre ever the button clicked
+    promise -> js feture . i can work with then 
+    the secend then work then the first one 
+    i can work with this in differant way which is 
+    async and await
+    async before the function and await before the fech and await return promise
+    dose the same as i do then
+    */
+  /* rather 
+   .then(response =>{
+      return response.json();
+    } i but
+    const response = await fetch('https://swapi.dev/api/films/');
+    and rather than the second then 
+    then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {}
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseData: movieData.release_date,
+          };
+        }); i but 
+        
+const data = await response.json();
+   const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+      */
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/');
+    const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
     const transformedMovies = data.results.map((movieData) => {
