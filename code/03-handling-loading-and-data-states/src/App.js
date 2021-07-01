690633320548812e4d46b8056e043988b910c545
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-07-01 18:01:26 +0300

    https with async and await and tell the user if the data loading or not by state

--- code/03-handling-loading-and-data-states/src/App.js
+++ code/03-handling-loading-and-data-states/src/App.js
@@ -5,7 +5,11 @@ import "./App.css";
 // still async
 function App() {
   const [movies, setMovies] = useState([]);
+  // i want to tell the user that the data is come from some other plase and now we dp other thng until its come
+  // i waite data loading  or not initaly false becuase on App comp run for the first time does not load data
+  // data loading when fetchMoviesHandler start so i will change the state to tru
   const [isLoading, setIsLoading] = useState(false);
+
   /* we are sending i get(select) request whre ever the button clicked
     promise -> js feture . i can work with then 
     the secend then work then the first one 
@@ -41,6 +45,7 @@ const data = await response.json();
     });
       */
   async function fetchMoviesHandler() {
+    // here i change the state while i start loading
     setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
@@ -54,8 +59,12 @@ const data = await response.json();
       };
     });
     setMovies(transformedMovies);
-    setIsLoading(false);
+    setIsLoading(false); // i done loading so im not loading now
   }
+  // i want to render the list if we are not loading  becuse now i have the data
+  // {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+  // if not loading and movie is empty array  {!isLoading && movies.length === 0 && <p>Found no movies.</p>}L
+  // if i looding  {isLoading && <p>Loading...</p>}
 
   return (
     <React.Fragment>
