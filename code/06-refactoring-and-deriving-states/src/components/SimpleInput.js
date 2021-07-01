ef133ac3e50c20e749ccfb5614896cb16cb9f6a1
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-07-01 23:50:34 +0300

    lain code to write validation with 3 way

--- code/06-refactoring-and-deriving-states/src/components/SimpleInput.js
+++ code/06-refactoring-and-deriving-states/src/components/SimpleInput.js
@@ -1,10 +1,17 @@
 import { useState } from 'react';
-
+/* combine the 3 way i alrady use submitted and losing focus andEvery Keystroke
+but i want to write it in a cleaner way 
+*/
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-
+/* 
+when new value is enter rhis comp will re evaluate
+so this constant -> enteredNameIsValid will have the lates value and touch state
+*/
+// if the enterd name is valid -> here i delete ever update state for enteredName
   const enteredNameIsValid = enteredName.trim() !== '';
+// its touched and not valid 
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = (event) => {
@@ -14,20 +21,22 @@ const SimpleInput = (props) => {
   const nameInputBlurHandler = event => {
     setEnteredNameTouched(true);
   };
+// this function re created when comp re evaluate so i can use enteredNameIsValid here becuse it have the most updated value 
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
     setEnteredNameTouched(true);
-
+// if invalid
     if (!enteredNameIsValid) {
       return;
     }
-
+// not need to set valid to true becuse noe already done 
     console.log(enteredName);
 
     // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
-    setEnteredName('');
+    setEnteredName('');// after submiting this make enteredNameIsValid = false so invalid input  to fix it i need to do this:
+    //this:
     setEnteredNameTouched(false);
   };
 
