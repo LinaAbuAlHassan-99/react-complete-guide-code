7eeb769fec132df8569eb4c6cdaa8aeba3f51475
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-07-04 19:43:31 +0300

    never change the state out side the return

--- code/08-how-to-work-with-redux-state-correctly/src/store/index.js
+++ code/08-how-to-work-with-redux-state-correctly/src/store/index.js
@@ -1,9 +1,30 @@
 import { createStore } from 'redux';
 
 const initialState = { counter: 0, showCounter: true };
+/*
+the object return from ower reducer
+alwyes we return new obj(new state) redux will replace the old state with it
+so the obj will no be marged with existing state it will replace it
+so that here i wrote every state in every return even if its not use it
+   return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+    // here i just increse so no need to toggel button but becuse redux overide the obj i need to put it her
 
+    So we must put all state in return 
+*/
 const counterReducer = (state = initialState, action) => {
+  
   if (action.type === 'increment') {
+// why i dont do this so dont write the all state in return every time?
+// beacuse when we work with redux we should never mutate (change) the existing state 
+//insted over ride it by return new obj 
+//becuse obj and [] work are refferance values
+    //state.counter++;// never change the state like this it my return side effect not wanted
+    //return state;
+
+//alwyes crete new obj like this (when ever i need to update i need to put it in return (create new obj))
     return {
       counter: state.counter + 1,
       showCounter: state.showCounter,
