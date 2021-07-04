45fe4ea59b7b818d747aee6a7b5d323d86793b7c
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-07-04 19:08:45 +0300

    redux class-based

--- code/05-redux-with-class-based-cmp/src/components/Counter.js
+++ code/05-redux-with-class-based-cmp/src/components/Counter.js
@@ -31,9 +31,12 @@ const Counter = () => {
 
 export default Counter;
 
+///import {Component} from 'react'
+///import { connect } from 'react-redux';
 // class Counter extends Component {
+
 //   incrementHandler() {
-//     this.props.increment();
+//     this.props.increment();// becuse of oure map in the second function
 //   }
 
 //   decrementHandler() {
@@ -41,7 +44,7 @@ export default Counter;
 //   }
 
 //   toggleCounterHandler() {}
-
+/// I need this so i refer to this class
 //   render() {
 //     return (
 //       <main className={classes.counter}>
@@ -56,13 +59,13 @@ export default Counter;
 //     );
 //   }
 // }
-
+/// reseve the redux state and return obj where the keys will reseve in counter and the values the logic to redux state (extract the data)
 // const mapStateToProps = state => {
 //   return {
 //     counter: state.counter
 //   };
 // }
-
+/// in counter comp we have this prop and then we can call dispatch
 // const mapDispatchToProps = dispatch => {
 //   return {
 //     increment: () => dispatch({ type: 'increment' }),
@@ -70,4 +73,7 @@ export default Counter;
 //   }
 // };
 
-// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);//conectwhen execute return new func and then execute this return function(Counter)
+//connect(function redux state to props which will pe resived, function that change the action)
+
+
