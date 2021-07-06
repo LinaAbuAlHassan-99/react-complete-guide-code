14fde1e1a66f26e1bc36ece1a5a60207d36bb4b9
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-07-06 15:44:11 +0300

    Query parameter to URL

--- code/18-working-with-query-params/src/App.js
+++ code/18-working-with-query-params/src/App.js
@@ -5,8 +5,20 @@ import QuoteDetail from './pages/QuoteDetail';
 import NewQuote from './pages/NewQuote';
 import NotFound from './pages/NotFound';
 import Layout from './components/layout/Layout';
+// quary parameter find it at the end of url
+// in some path i found this ? and after it i can pass some data(parameter)
+// The regular parameter like /:quoteId arr manditory only if we have id (this segmant /:quoteId)
+//where quary parameter are optinal and its not affect the route match but if found a match will has access the quary data and could change the pehavier
+// ex of changing pehavire 
+/*
+might implemant sort my quote by id as assending or desending
+save if i sort it assending or dessinding in url if the user use the url without the quary will get the dufult (unsort page)
+*/
+// i first start in QuteList.js
 
 function App() {
+
+
   return (
     <Layout>
       <Switch>
--- code/18-working-with-query-params/src/components/quotes/QuoteList.js
+++ code/18-working-with-query-params/src/components/quotes/QuoteList.js
@@ -1,9 +1,9 @@
 import { Fragment } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';// ro reade quary in url
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
-
+// sort function 
 const sortQuotes = (quotes, ascending) => {
   return quotes.sort((quoteA, quoteB) => {
     if (ascending) {
@@ -16,18 +16,27 @@ const sortQuotes = (quotes, ascending) => {
 
 const QuoteList = (props) => {
   const history = useHistory();
+  //Read the quary parameter and act depend on it -> asc or desc
   const location = useLocation();
-
-  const queryParams = new URLSearchParams(location.search);
-
+// search hold quary data 
+//URLSearchParams bulid in constructer (defult js class)
+  const queryParams = new URLSearchParams(location.search);//its value here asc or desc
+//sort becuse we have it as key in our quary parameter
   const isSortingAscending = queryParams.get('sort') === 'asc';
-
+// to sort it i call sort function (data (quots)from props, order asc or desc(boll))
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
-
+// when sort button clicked
   const changeSortingHandler = () => {
-    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+    //update the quary paramter in url
+    //update shared url
+    //set our new path 
+    //sort-> i can name it what i name
+    //push this state make the comp re render
+    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));// if i sorting in asc i want to swich to desc else asc
+  // beacuse this will re eva luate every time the path change so will re evaluate the comp so sortedQuotes changed --> have the correct sort value
   };
-
+  //Sort {isSortingAscending ? 'Descending' : 'Ascending'} if i asc i want to change it to desinding when i click the button and the revverce if i am descinding
+  // {sortedQuotes.map((quote) =>  i use sorted data
   return (
     <Fragment>
       <div className={classes.sorting}>
