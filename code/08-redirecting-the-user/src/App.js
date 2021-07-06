16586e8857b09e51b0100e4190b7242a1a5ab119
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-07-06 11:45:06 +0300

    Redirect

--- code/08-redirecting-the-user/src/App.js
+++ code/08-redirecting-the-user/src/App.js
@@ -4,15 +4,18 @@ import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import ProductDetail from './pages/ProductDetail';
 import MainHeader from './components/MainHeader';
-
+// Redirect the user // if i visit my domai/ nothing then i dont see any page(i see the origin one) put i dont have erreor
+//here we can / to welcom <Route path='/' exact>
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
         <Switch>
-          <Route path='/' exact>
-            <Redirect to='/welcome' />
+        // if i start nothinf '/' i will see the wlcome then rediricting to its rout the welcome 
+        // from nothing i redirected to welcome  
+        <Route path='/' exact>// not treded as start i need exact
+            <Redirect to='/welcome' />//redirect the user to some where else
           </Route>
           <Route path='/welcome'>
             <Welcome />
