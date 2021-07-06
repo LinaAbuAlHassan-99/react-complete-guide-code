61700f7e2be85a4b9307bf5fe7d5037dc6bf76e8
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-07-06 11:27:48 +0300

    use switch so i can render the path i want

--- code/06-using-switch-and-exact/src/App.js
+++ code/06-using-switch-and-exact/src/App.js
@@ -1,4 +1,5 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';///this to not active all the all route at the same time 
+//just what i want to active
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -10,14 +11,20 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        <Switch>
+        <Switch> // the first match(start) will render it
           <Route path='/welcome'>
             <Welcome />
           </Route>
-          <Route path='/products' exact>
-            <Products />
+          <Route path='/products' exact>// will render this when click any products beacuse the start path of them is /products/
+          //just i want to structer out path just (i want the pefor path then another path come after it)
+          //to fix it i can change the order  
+          <Products />
           </Route>
+          ///products/ its the start
           <Route path='/products/:productId'>
+          //or i can add exact prop so only if i had the exact match load it
+          // i want from products to onether page 
+          
             <ProductDetail />
           </Route>
         </Switch>
--- code/06-using-switch-and-exact/src/pages/Products.js
+++ code/06-using-switch-and-exact/src/pages/Products.js
@@ -1,12 +1,21 @@
+// turn my product to link not nav link becuse i dont need active styling
 import { Link } from 'react-router-dom';
+// p1,p2,p3 keys
 
+// now i load this but under the product links not in a new page (not leaving the product page)
+//thats beacuse how react match the path that the same start of path loaded at the same time
+/*
+and this good if i need to have the details just under the comp(multiple route active at the same time)
+put if i want new page i need to do this(one active rout on the time)
+ in App.js
+*/
 const Products = () => {
   return (
     <section>
       <h1>The Products Page</h1>
       <ul>
         <li>
-          <Link to='/products/p1'>A Book</Link>
+          <Link to='/products/p1'>A Book</Link>//P1 -> /:productId
         </li>
         <li>
           <Link to='/products/p2'>A Carpet</Link>
