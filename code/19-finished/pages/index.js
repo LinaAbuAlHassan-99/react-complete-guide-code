43d599149e455d7aefe42a8196ed16c6a0b27f5b
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-07-07 18:04:32 +0300

    add head->title+meta

--- code/19-finished/pages/index.js
+++ code/19-finished/pages/index.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import Head from 'next/head';
+import Head from 'next/head';//add head and write title ,meta
 import { MongoClient } from 'mongodb';
 
 import MeetupList from '../components/meetups/MeetupList';
