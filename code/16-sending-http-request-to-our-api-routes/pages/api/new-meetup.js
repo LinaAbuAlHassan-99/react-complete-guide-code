e4bdca04127ae2e71ba3575d95636ffbfa94adc6
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-07-07 17:42:46 +0300

    API route ,http request

--- code/16-sending-http-request-to-our-api-routes/pages/api/new-meetup.js
+++ code/16-sending-http-request-to-our-api-routes/pages/api/new-meetup.js
@@ -1,26 +1,35 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb'; //DB mango Altise
 
-// /api/new-meetup
+// /api/new-meetup // if request trigger this path then this fun will execute
 // POST /api/new-meetup
 
-async function handler(req, res) {
-  if (req.method === 'POST') {
-    const data = req.body;
 
+// server side code only run in server 
+//never contain react comp
+
+
+async function handler(req, res) {
+  if (req.method === 'POST') {//only post request will treger here
+    const data = req.body;//data i want to store
+// i can use try catch to handel error
+// connect to our DB
+//becuse its alowys run on server side then its sequre i can add my pass
+//MongoClient obj allow us to connect i need to put the username and password //meetups data base name
     const client = await MongoClient.connect(
       'mongodb+srv://maximilian:arlAapzPqFyo4xUk@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority'
     );
-    const db = client.db();
 
-    const meetupsCollection = db.collection('meetups');
+    const db = client.db();//hold of DB
+//'meetups' the name of collection i can name it what i want
+    const meetupsCollection = db.collection('meetups');// work with collection (attribut,tabels) and doc (enty)
 
-    const result = await meetupsCollection.insertOne(data);
+    const result = await meetupsCollection.insertOne(data);//insert data to DB
 
-    console.log(result);
+    console.log(result);//result obj of genarated id
 
     client.close();
-
-    res.status(201).json({ message: 'Meetup inserted!' });
+      //return responce 
+    res.status(201).json({ message: 'Meetup inserted!' });//201 to inform insert secsesfoly
   }
 }
 
--- code/16-sending-http-request-to-our-api-routes/pages/new-meetup/index.js
+++ code/16-sending-http-request-to-our-api-routes/pages/new-meetup/index.js
@@ -1,15 +1,15 @@
 // our-domain.com/new-meetup
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';//
 
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
-
+//send request apiroute
 function NewMeetupPage() {
   const router = useRouter();
-
+// as normal request 
   async function addMeetupHandler(enteredMeetupData) {
-    const response = await fetch('/api/new-meetup', {
+    const response = await fetch('/api/new-meetup', {//the path of apirouter
       method: 'POST',
-      body: JSON.stringify(enteredMeetupData),
+      body: JSON.stringify(enteredMeetupData),//data i want to store in database
       headers: {
         'Content-Type': 'application/json'
       }
@@ -19,7 +19,7 @@ function NewMeetupPage() {
 
     console.log(data);
 
-    router.push('/');
+    router.push('/');//to navigate to the root after response 
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />
