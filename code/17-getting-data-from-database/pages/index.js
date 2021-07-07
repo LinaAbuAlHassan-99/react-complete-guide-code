60bce783b8a7b2e432280d60a2c0ab0223daf1b7
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-07-07 17:51:34 +0300

    Get request

--- code/17-getting-data-from-database/pages/index.js
+++ code/17-getting-data-from-database/pages/index.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb';//will not execute in cliant becuse i use it in server side(getStaticProps)
 
 import MeetupList from '../components/meetups/MeetupList';
 
@@ -19,6 +19,8 @@ function HomePage(props) {
 //   };
 // }
 
+//get the stored data (GET) fetch('/api/meetups)// but this redundunt becuse we send req to our API
+//we dont need to end request we use the code
 export async function getStaticProps() {
   // fetch data from an API
   const client = await MongoClient.connect(
@@ -28,12 +30,12 @@ export async function getStaticProps() {
 
   const meetupsCollection = db.collection('meetups');
 
-  const meetups = await meetupsCollection.find().toArray();
+  const meetups = await meetupsCollection.find().toArray();//find all the document(entry).to array to have array doc 
 
-  client.close();
+  client.close();//close the connection with DB
 
   return {
-    props: {
+    props: {// i cant return meetups directly i need to transfare it also becuse i d is obj and give us error so i need to string in it
       meetups: meetups.map((meetup) => ({
         title: meetup.title,
         address: meetup.address,
