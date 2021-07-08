6f17aaee128728f9a945c5d97b9092f2f85a12cf
LinaAbuAlHassan-99 <85435180+LinaAbuAlHassan-99@users.noreply.github.com>
2021-07-08 15:51:23 +0300

    i just add the context file

--- code/07-new-starting-project/src/pages/Favorites.js
+++ code/07-new-starting-project/src/pages/Favorites.js
@@ -1,4 +1,11 @@
+
+// i want to use useContext -> to share data from differant file like redux
+// i can left data up but i want context to becoume more reaseir
+//very long chain and when project become more and more bigger will be harder
+
 const FavoritesPage = () => {
+
+  
     return <div>Favorites Page</div>;
   };
   export default FavoritesPage;
---
+++ code/07-new-starting-project/src/store/favorites-context.js
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react";
+//createContext return components the argument is initial value for the comp // it can be any thing i want
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+});
+
+//comp will provide this context need this values, responceplr to update context value
+function FavoritesContextProvider(props) {
+  const [userFavo, setUserFave] = useState([]);
+
+  function addFavoHandler(favoMeetup){
+    setUserFave((PrevUserFavo)=>{//if i depend on previous state//becuse react scudual update so maby the prvious value not in yet
+        return PrevUserFavo.concat(favoMeetup);//now we get the latest update 
+    });//(userFavo.concat(favoMeetup));// concat  like push put return new array with the new values
+  }
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites(prevUserFavorites => {
+      return prevUserFavorites.filter(meetup => meetup.id !== meetupId);// i need to remove the item with id (the return array will hold all put the removed)
+    });
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+      //some want function execute for every item return true all false so i can komw that if i have a meetup with that id in favoiret
+    return userFavorites.some(meetup => meetup.id === meetupId);
+  }
+  //function itemIsFavoHandler(){} // to knoe if the item is favo or not
+
+  const context = {
+    favorites: userFavo,
+    totalFavorites: userFavo.length,
+  }; //this object will passes as value so i can update the values in createContext
+
+  // wrap this provider around children so when i wrap any comp with this comp it can use the data
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+}
+export default FavoritesContextProvider;
